ef9f0a198d0651cce698f1e55aff384bbab0774a
Aries Wibowo <riespratama@gmail.com>
2022-12-05 21:15:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,16 @@ st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?l
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Aries P. Wibowo')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('App Developer and Tech Enthusiast who love coffee, It does not always take sophisticated technology to make life better.')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://www.youtube.com/@createwitharies ', 'Create With Aries YouTube channel', icon_size)
+st_button('instagram', 'https://instagram.com/riespratama', 'My Personal Instagram', icon_size)
+st_button('medium', 'https://riespratama.com', 'Read my Blogs', icon_size)
+st_button('twitter', 'https://twitter.com/riespratama/', 'Follow me on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/aries-wibowo/', 'Follow me on LinkedIn', icon_size)
+st_button('newsletter', '#', 'Sign up for my Newsletter', icon_size)
+st_button('cup', '#', 'Buy me a Coffee', icon_size)
